c9a387bed4f2b56c077caa696a3232ddfa37249d
Rado M Kirilchev <s1452923@sms.ed.ac.uk>
2018-09-25 15:01:39 +0100

    Activity refactoring complete, making things a lot more elegant. Must now make registration UI.

--- coinz/app/src/main/java/eu/zerovector/coinz/LoginActivity.kt
+++ coinz/app/src/main/java/eu/zerovector/coinz/LoginActivity.kt
@@ -1,81 +1,18 @@
 package eu.zerovector.coinz
 
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.view.Window
-import kotlinx.android.synthetic.main.activity_login.*
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
+
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
 
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_login)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         title = "COINZ Login"
 
-
-        // Set up the swipe detection for the registration.
-
     }
 
-
-    ///////// UI HIDING GARBAGE
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-
-        hide()
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-
-        if (hasFocus) delayedHide(100)
-    }
-
-
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
-
-        mHideHandler.postDelayed(mHidePart2Runnable, 300.toLong())
-
-        // Hide the status bar.
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        actionBar?.hide()
-    }
-
-
-    private fun delayedHide(delayMillis: Int) {
-        mHideHandler.removeCallbacks(mHideRunnable)
-        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
-    }
-
-    private val mHideRunnable = Runnable { hide() }
-
-    private val mHideHandler = Handler()
-    private val mHidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
-
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        layoutLoginScreen.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION /*or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION*/
-    }
 }
--- coinz/app/src/main/java/eu/zerovector/coinz/MainMenuActivity.kt
+++ coinz/app/src/main/java/eu/zerovector/coinz/MainMenuActivity.kt
@@ -2,23 +2,17 @@ package eu.zerovector.coinz
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.Window
 import kotlinx.android.synthetic.main.activity_main_menu.*
 
-class MainMenuActivity : AppCompatActivity() {
+class MainMenuActivity : BaseFullscreenActivity() {
 
     var splashAnimating = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main_menu)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         title = "COINZ"
 
         // Set up the user interaction to manually show or hide the system UI.
@@ -41,66 +35,11 @@ class MainMenuActivity : AppCompatActivity() {
     }
 
 
-
-
-    fun onLoginClicked() {
+    fun onLoginClicked(view : View) {
         startActivity(Intent(this@MainMenuActivity, LoginActivity::class.java))
     }
 
-    fun onRegisterClicked() {
+    fun onRegisterClicked(view : View) {
         startActivity(Intent(this@MainMenuActivity, RegisterActivity::class.java))
-    }
-
-
-
-    ///////// UI HIDING GARBAGE
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-
-        hide()
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-
-        if (hasFocus) delayedHide(100)
-    }
-
-
-    private fun hide() {
-        // Hide UI first
-        supportActionBar?.hide()
-
-        mHideHandler.postDelayed(mHidePart2Runnable, 300.toLong())
-
-        // Hide the status bar.
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        // Remember that you should never show the action bar if the
-        // status bar is hidden, so hide that too if necessary.
-        actionBar?.hide()
-    }
-
-
-    private fun delayedHide(delayMillis: Int) {
-        mHideHandler.removeCallbacks(mHideRunnable)
-        mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
-    }
-
-    private val mHideRunnable = Runnable { hide() }
-
-    private val mHideHandler = Handler()
-    private val mHidePart2Runnable = Runnable {
-        // Delayed removal of status and navigation bar
-
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        layoutMainMenu.systemUiVisibility =
-                View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION /*or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION*/
     }
 }
