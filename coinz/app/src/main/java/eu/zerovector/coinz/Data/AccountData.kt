f330cf1b89dfc666d7e40b175d85bd8555e57c74
Rado M Kirilchev <s1452923@sms.ed.ac.uk>
2018-10-08 13:41:24 +0100

    Custom launcher icons made. Changed starting amounts of GOLD and compute to balance some stuff out.

--- coinz/app/src/main/java/eu/zerovector/coinz/Data/AccountData.kt
+++ coinz/app/src/main/java/eu/zerovector/coinz/Data/AccountData.kt
@@ -13,13 +13,13 @@ data class AccountData (
         var experience: Int = 0,
 
         // Processing power, a.k.a. "compute", is the currency required to decrypt the daily messages and thus win the "war"
-        var compute: Int = 0,
+        var compute: Int = 200,
 
         var dailyDepositsLeft: Int = 2500,
         var dailyMessagesDecrypted: Long = 0, // enforce 64 bits on this one, just in case, since it's a bit-mask
 
         // Bank stuff
-        var balanceGold: Int = 20000,
+        var balanceGold: Int = 30000,
         var balances: Map.Wallet = Map.Wallet(), // we're recycling the Map.Rates class, which holds one double per currency
         // Spares
         var spares: Map.Wallet = Map.Wallet()
